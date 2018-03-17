07ab23256bc419981c7b9d7b661ab15d279fcbfb
devgaram <joingaram@naver.com>
2018-03-18 02:28:50 +0900

    vuejs 공부 ~조건부렌더링까지

---
+++ 005_ConditionalRendering_template.vue
@@ -0,0 +1,24 @@
+<template v-if="loginType === 'username'">
+  <label>사용자 이름</label>
+  <input placeholder="사용자 이름을 입력하세요">
+</template>
+<template v-else>
+  <label>이메일</label>
+  <input placeholder="이메일 주소를 입력하세요">
+</template>
+<button @click="toggleLoginType"></button>
+
+<script>
+module.exports = {
+	data : function(){
+		return {
+			loginType : 'username'
+		}
+	},
+	methods : {
+		toggleLoginType : function(){
+			return this.loginType = this.loginType === 'username' ?'email' : 'username';
+		}
+	}
+}
+</script>
